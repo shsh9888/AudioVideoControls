d4f5a8a7cbb35f22fce804547973ac1468df552e
Shravan Shetty <shsh9888@colorado.edu>
2020-04-16 21:08:10 -0600

    Added more audio controls including volume control and audio length control

--- app/src/main/java/com/example/audiovideo/MainActivity.java
+++ app/src/main/java/com/example/audiovideo/MainActivity.java
@@ -9,17 +9,25 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.MediaController;
+import android.widget.SeekBar;
 import android.widget.VideoView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //initializing the video View
-
+        /*
+       //setting audio service
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 20, 0);
         VideoView videoView = (VideoView) findViewById(R.id.myVideo);
         String path = "android.resource://" + getPackageName() + "/" + R.raw.child;
         videoView.setVideoURI(Uri.parse(path));
@@ -27,14 +35,62 @@ public class MainActivity extends AppCompatActivity {
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
         videoView.setMediaController(mediaController);
+        */
 
-        //setting audio manager
-        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 20, 0);
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.waterfall);
+        SeekBar volumeControl = (SeekBar) findViewById(R.id.volumeControl);
+        final SeekBar audioPlayControl = (SeekBar) findViewById(R.id.audioPlayControl);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVol = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        volumeControl.setMax(maxVol);
+        volumeControl.setProgress(currentVol);
+        audioPlayControl.setMax(mediaPlayer.getDuration());
+        audioPlayControl.setProgress(mediaPlayer.getCurrentPosition());
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                System.out.println("seekbar " + progress);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        audioPlayControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mediaPlayer.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                audioPlayControl.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 1000 );
     }
-
-
 
     public void play(View view) {
         mediaPlayer.start();
